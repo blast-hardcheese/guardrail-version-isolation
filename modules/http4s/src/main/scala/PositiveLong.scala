ff2cb6eebc74fc5ff1ec27761dd64540ce75c2fc
Devon Stewart <dstewart@twilio.com>
2019-01-13 13:43:26 -0800

    Adding PositiveLong

---
+++ modules/http4s/src/main/scala/PositiveLong.scala
@@ -0,0 +1,11 @@
+package support
+
+import clients.http4s.{ Implicits => Http4sImplicits }
+import io.circe.Decoder
+
+class PositiveLong private (val value: Long) extends AnyVal
+object PositiveLong {
+  def apply(value: Long): Option[PositiveLong] = if (value >= 0) Some(new PositiveLong(value)) else None
+  implicit val http4sShowable                  = Http4sImplicits.Show.build[PositiveLong](_.value.toString())
+  implicit val decodePositiveLong              = Decoder.decodeLong.emap(num => PositiveLong.apply(num).toRight(s"${num} is not positive"))
+}
